53a07451971e53ee088bc3ddcdce6e57fd8588fa
Shubham Gaikwad <shubhamtechgeek@gmail.com>
2023-09-26 20:39:05 +0530

    simple handler message

---
+++ recordManagement/src/main/java/com/employee/recordManagement/Exception/InfyEmployeeException.java
@@ -0,0 +1,7 @@
+package com.employee.recordManagement.Exception;
+
+public class InfyEmployeeException extends Exception{
+    public InfyEmployeeException(String message){
+        super(message);
+    }
+}
