c0fb7f9b2eb84f14a9fb84f3953f586033cef999
Shubham Gaikwad <shubhamtechgeek@gmail.com>
2023-09-26 21:39:02 +0530

    deleteEmployee Id update 2

--- recordManagement/src/main/java/com/employee/recordManagement/Service/employeeService.java
+++ recordManagement/src/main/java/com/employee/recordManagement/Service/employeeService.java
@@ -2,16 +2,17 @@ package com.employee.recordManagement.Service;
 
 import com.employee.recordManagement.DTO.employeeDTO;
 import com.employee.recordManagement.Entity.Employee;
+import com.employee.recordManagement.Exception.InfyEmployeeException;
 
 import java.util.List;
 
 public interface employeeService {
 
 
-    public void addEmployee(employeeDTO emp) throws Exception;
-    public void deleteEmployee(employeeDTO emp) throws Exception;
-    public void updateEmployee(employeeDTO emp) throws Exception;
-    public Employee findByEmpIdOrEmail(Integer identify) throws Exception;
-    public List<Employee> getAllEmployees() throws Exception;
+    public void addEmployee(employeeDTO emp) throws InfyEmployeeException;
+    public void deleteEmployee(Integer identify) throws InfyEmployeeException;
+    public void updateEmployee(employeeDTO emp) throws InfyEmployeeException;
+    public Employee findByEmpIdOrEmail(Integer identify) throws InfyEmployeeException;
+    public List<Employee> getAllEmployees() throws InfyEmployeeException;
 
 }
