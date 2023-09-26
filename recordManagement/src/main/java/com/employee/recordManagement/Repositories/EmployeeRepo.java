d7a3d7d08a99a86c184fd6eb3b54478bf4f106a5
Shubham Gaikwad <shubhamtechgeek@gmail.com>
2023-09-26 20:40:47 +0530

    EmployeeRepo with Custom impl for finding emp based on email and emp id

--- recordManagement/src/main/java/com/employee/recordManagement/Repositories/EmployeeRepo.java
+++ recordManagement/src/main/java/com/employee/recordManagement/Repositories/EmployeeRepo.java
@@ -1,9 +1,11 @@
 package com.employee.recordManagement.Repositories;
 
+import com.employee.recordManagement.DTO.employeeDTO;
 import com.employee.recordManagement.Entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepo extends JpaRepository <Employee, Integer>{
 
+    Employee findEmployeeByEmpIdOrEmail(Integer empId, String email);
 
 }
