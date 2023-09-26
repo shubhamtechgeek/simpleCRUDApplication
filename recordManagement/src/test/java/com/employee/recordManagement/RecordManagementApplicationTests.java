de357d5f67dbb47f96680bffcbcf730598cd6224
Shubham Gaikwad <shubhamtechgeek@gmail.com>
2023-09-26 17:56:17 +0530

    initial commit

---
+++ recordManagement/src/test/java/com/employee/recordManagement/RecordManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee.recordManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
